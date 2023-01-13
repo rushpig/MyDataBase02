183ab529810a9bd8c0507d7795b79ffa38b00006
liangnv <liangnv.com>
2023-01-13 23:28:33 +0800

    dm

---
+++ src/main/java/backend/dm/dataItem/DataItem.java
@@ -0,0 +1,50 @@
+package backend.dm.dataItem;
+
+import java.util.Arrays;
+
+import backend.common.SubArray;
+import backend.dm.DataManagerImpl;
+import backend.dm.page.Page;
+import backend.utils.Parser;
+import backend.utils.Types;
+import com.google.common.primitives.Bytes;
+
+
+
+public interface DataItem {
+    SubArray data();
+    
+    void before();
+    void unBefore();
+    void after(long xid);
+    void release();
+
+    void lock();
+    void unlock();
+    void rLock();
+    void rUnLock();
+
+    Page page();
+    long getUid();
+    byte[] getOldRaw();
+    SubArray getRaw();
+
+    public static byte[] wrapDataItemRaw(byte[] raw) {
+        byte[] valid = new byte[1];
+        byte[] size = Parser.short2Byte((short)raw.length);
+        return Bytes.concat(valid, size, raw);
+    }
+
+    // 从页面的offset处解析处dataitem
+    public static DataItem parseDataItem(Page pg, short offset, DataManagerImpl dm) {
+        byte[] raw = pg.getData();
+        short size = Parser.parseShort(Arrays.copyOfRange(raw, offset+DataItemImpl.OF_SIZE, offset+DataItemImpl.OF_DATA));
+        short length = (short)(size + DataItemImpl.OF_DATA);
+        long uid = Types.addressToUid(pg.getPageNumber(), offset);
+        return new DataItemImpl(new SubArray(raw, offset, offset+length), new byte[length], pg, uid, dm);
+    }
+
+    public static void setDataItemRawInvalid(byte[] raw) {
+        raw[DataItemImpl.OF_VALID] = (byte)1;
+    }
+}
